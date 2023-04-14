6c49eda0950e9acd8a7248a750c9c866a4261873
labricecat <labricecat@duck.com>
2023-04-14 19:55:34 +0200

    hitfix: cmake not linking ncurses

---
+++ build/CMakeFiles/purrdbg.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/purrdbg.dir/main.cpp.o"
+  "CMakeFiles/purrdbg.dir/main.cpp.o.d"
+  "purrdbg"
+  "purrdbg.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/purrdbg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
